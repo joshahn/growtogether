a7bdb46ea097838943e108b6cee7e5a769ace023
Jason Lu <jason.j.lu@oracle.com>
2017-06-03 16:46:56 -0700

    Add postgressql db and implemented user service

---
+++ target/generated-sources/jsonschema/users/User.java
@@ -0,0 +1,177 @@
+
+package users;
+
+import javax.annotation.Generated;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonPropertyOrder;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+
+/**
+ * User Schema
+ * <p>
+ * The details of a particular User
+ * 
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "id",
+    "firstName",
+    "lastName",
+    "email"
+})
+public class User {
+
+    /**
+     * ID of a User
+     * 
+     */
+    @JsonProperty("id")
+    private Integer id;
+    /**
+     * First Name of a user
+     * 
+     */
+    @JsonProperty("firstName")
+    private String firstName;
+    /**
+     * Last Name of a user
+     * 
+     */
+    @JsonProperty("lastName")
+    private String lastName;
+    /**
+     * Email of a user
+     * 
+     */
+    @JsonProperty("email")
+    private String email;
+
+    /**
+     * ID of a User
+     * 
+     * @return
+     *     The id
+     */
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * ID of a User
+     * 
+     * @param id
+     *     The id
+     */
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * First Name of a user
+     * 
+     * @return
+     *     The firstName
+     */
+    @JsonProperty("firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * First Name of a user
+     * 
+     * @param firstName
+     *     The firstName
+     */
+    @JsonProperty("firstName")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public User withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    /**
+     * Last Name of a user
+     * 
+     * @return
+     *     The lastName
+     */
+    @JsonProperty("lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Last Name of a user
+     * 
+     * @param lastName
+     *     The lastName
+     */
+    @JsonProperty("lastName")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    /**
+     * Email of a user
+     * 
+     * @return
+     *     The email
+     */
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Email of a user
+     * 
+     * @param email
+     *     The email
+     */
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return EqualsBuilder.reflectionEquals(this, other);
+    }
+
+}
