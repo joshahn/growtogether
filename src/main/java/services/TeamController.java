c49f1aa8bd8d354fca098e60528785d8309b45b6
Joshua Ahn <joshuaahn@localadmins-MacBook-Pro.local>
2017-06-17 21:37:43 -0700

    Adding team endpoints
    - Added GET point to get all persons (and tasks)
    for a specific team id

---
+++ src/main/java/services/TeamController.java
@@ -0,0 +1,52 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.transaction.annotation.Transactional;
+
+import errors.ServiceException;
+import json.Person;
+import json.Task;
+import json.Team;
+
+
+@Controller
+@Transactional
+@RequestMapping("/api/teams")
+public class TeamController extends BaseController {
+	@RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody List<Person> getTeam(@RequestParam(value="id", required=true) int id) {
+        try (Connection connection = getConnection()) {
+            connection.setAutoCommit(false);
+            String selectStatement = "SELECT p.* FROM person p "
+            		+ "LEFT JOIN teampersons tp ON (p.id = tp.person_id) "
+            		+ "LEFT JOIN team t ON (t.id = tp.team_id) "
+            		+ "WHERE t.id = ?;";
+            PreparedStatement pstmt = connection.prepareStatement(selectStatement);
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            List<Person> persons = new ArrayList<Person>();
+            while (rs.next()) {
+            	Person person = new Person();
+            	String email = rs.getString("email");
+            	PersonsController.setPersonAndTask(person, email);
+                persons.add(person);
+            }
+            rs.close();
+            pstmt.close();
+            connection.close();
+            return persons;
+        } catch (Exception e) {
+            throw new ServiceException("Error while retrieving list of all tasks");
+        }
+    }
+}
